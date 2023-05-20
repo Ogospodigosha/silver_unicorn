7d8232a352ba2c554e3aeaf9dc5e37f67410c7c5
Ogospodigosha <odintsovis@hotmail.com>
2023-05-20 22:03:07 +0300

    add prettier, eslint config

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,9 @@
-import React from "react";
-import './App.css'
-export const App = () =>{
-    return <div className={'blue'}>Gosha</div>
-}
+import React from 'react';
+import './App.css';
+export const App = () => {
+  return (
+    <div style={{ backgroundColor: 'green' }}>
+      <div style={{ color: 'red' }}>Gosha</div>
+    </div>
+  );
+};
