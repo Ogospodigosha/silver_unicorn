be87b607c84f42fb2b6bcda5147174765083a164
Ogospodigosha <odintsovis@hotmail.com>
2023-05-23 19:10:56 +0300

    add: pages

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -2,13 +2,19 @@ import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
+import { NewsPage } from 'pages/NewsPage';
+import { TrashPage } from 'pages/TrashPage';
+import { CatalogPage } from 'pages/CatalogPage';
 
 export const AppRouter = () => {
   return (
     <Suspense fallback={<div>loading</div>}>
       <Routes>
-        <Route path={'/about'} element={<AboutPage />} />
+        <Route path={'/trash'} element={<TrashPage />} />
+        <Route path={'/news'} element={<NewsPage />} />
         <Route path={'/'} element={<MainPage />} />
+        <Route path={'/about'} element={<AboutPage />} />
+        <Route path={'/catalog'} element={<CatalogPage />} />
       </Routes>
     </Suspense>
   );
