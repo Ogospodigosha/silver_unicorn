b611361233c253d3637c24052fe41a279aef0d83
Ogospodigosha <odintsovis@hotmail.com>
2023-05-24 18:26:38 +0300

    add: Button, file-loader

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
 
 export const MainPage = () => {
-  return <div>MAIN PAGE1</div>;
+  return (
+    <div>
+      <div>MAIN PAGE1</div>
+      <Button theme={ThemeButton.ClEAR}>Читать</Button>
+    </div>
+  );
 };
 export default MainPage;
