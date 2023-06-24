946af5149ad856a3c9eb7a1f546ceeff904021e6
atastemi <azamat.chimchikov@gmail.com>
2023-06-24 12:31:22 +0600

    :adding card

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './styles/index.scss';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
-
+import { CardItem } from 'widgets/CardItem';
 export const App = () => {
   return (
     <div className={'app'}>
       <Navbar />
+      <CardItem />
       <AppRouter />
     </div>
   );
--- src/widgets/CardItem/ui/CardItem.tsx
+++ src/widgets/CardItem/ui/CardItem.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/ClassNames';
 import cls from './CardItem.module.scss';
-
+import heart from '../../../shared/assets/icons/ph_heart-thin.svg';
 
 interface CardItem {
   className?: string;
@@ -9,8 +9,19 @@ interface CardItem {
 
 export const CardItem: FC<CardItem> = ({ className }) => {
   return (
-    <div className={classNames(cls.navbar, {}, [className])}>
-        
+    <div className={classNames(cls.cardItem, {}, [className])}>
+      <div className={cls.cardItemBanner}>
+        {/* <img
+          className={cls.cardImage}
+          src=""
+          alt=""
+        /> */}
+        <img src={heart} className={cls.heart} alt="" />
+      </div>
+      <div className={cls.cardText}>
+        <h6 className={cls.cardTitle}>SHOWMASTER Вasic Pro</h6>
+        <h6 className={cls.cardPrice}>5 000 ₽</h6>
+      </div>
     </div>
   );
 };
