3b764be77ee1d2ffee6e4b6803b1c6084bb1ae64
AleksARPV <ascanalex@gmail.com>
2023-07-13 16:21:12 +0400

    Сделано: 1. Проведена декомпозиция webpack.config.ts (папка config). Произведеная типизация конфига и переведено расширение из js в ts 2. Проведена типизация декомпозированных компонентов вебпак конфига 3. Добавлен парсер в .eslintrc 4. Скорректирован корневой index.tsx 5. Проведена декомпозиция babel-loader 6. Сделан переход с двух loaders (ts-loader & babel-loader) на один - babel-loader 7. Скорректирвоан babel.config.json 8. Добавлен плагин для css-loader - MiniCssExtractPlugin. Проведена декомпозиция css-loader 9. Скорректирован tsconfig.json 10. Скорректирован package.json (изменены некоторые скрипты для сборки/запуска проекта, большая часть пакетов перенесена из dependencies в devDependencies)

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import ReactDom from 'react-dom';
-import { App } from './app/App';
-import { HashRouter } from 'react-router-dom';
+import { App } from "app/App";
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDom.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-  document.getElementById('root')
-);
+// ReactDom.render(
+//   <HashRouter>
+//     <App />
+//   </HashRouter>,
+//   document.getElementById('root')
+// );
+
+const container = document.getElementById('root')
+
+if (!container) {
+  throw new Error('Container root not found')
+}
+
+const root = createRoot(container)
+
+root.render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+)
